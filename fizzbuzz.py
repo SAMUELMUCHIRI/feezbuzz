dfafd0e76a7de03caa3404ba67fe09a6e3fbbc28
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2022-07-13 10:59:09 +0300

    feez buzz methods
    Signed-off-by: SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+#first solution
+for i in range(1,101):
+    if i%3==0 and i%5==0:
+        print("feezbuz")
+    elif i%3==0:
+        print("feez")
+    elif i%5==0:
+        print("buzz")
+    else:
+        print(i)
+#second solution 
+for i in range(1,101):
+    print("feez"*(i%3<1)+(i%5<1)*"buzz" or i)
